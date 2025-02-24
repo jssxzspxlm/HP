38a44d276dc68e3740277b16a62b10e93ce97ce0
MeoLiu <meoliu@126.com>
2025-02-24 23:52:40 +0800

    Start

---
+++ TexasPoker/src/main/java/com/hp/TexasPoker/TexasPokerApplication.java
@@ -0,0 +1,13 @@
+package com.hp.TexasPoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexasPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexasPokerApplication.class, args);
+	}
+
+}
---
+++ TexasPoker/src/test/java/com/hp/TexasPoker/TexasPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hp.TexasPoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexasPokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
